f72a65bf5073968665714053791d0bf2cbbfbec5
KateRadziukevich <kattya-0@mail.ru>
2022-11-07 15:07:22 +0300

    Домашнее задание 4
    Добавила скрипт для добавления новых элементов по нажатию Enter, без добавления пустых элементов.
    При клике на элемент применяется и отменяется класс, который зачеркивает элемент.

--- script.js
+++ script.js
@@ -7,3 +7,29 @@
 /* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
 
 /* Очищать input после добавления нового элемента в список */
+
+
+const input = document.querySelector('#input');
+const items = document.querySelector('#items');
+
+input.addEventListener('keydown', function (event) {
+    if (event.key == 'Enter') {
+        const li = document.createElement('li');
+        li.textContent = input.value;
+        
+        if (input.value != '') {
+            items.append(li);
+        }
+
+        input.value = "";
+        
+    }
+
+    const elements = document.querySelectorAll('li');
+    for (let item of elements) {
+      item.addEventListener ('click', function() {
+      item.classList.toggle ('done');
+     });
+};
+    
+});
